4d7dc537ca0fb918d6d6e4b7a071085b63d7338e
Tahmid Zaman <tahmid198@yahoo.com>
2021-03-06 14:08:07 -0500

    Initial Commit

---
+++ Mario Kart Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mario Kart Demo
+//
+//  Created by Tahmid Zaman on 3/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
